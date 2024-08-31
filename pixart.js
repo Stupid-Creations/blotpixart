6262d82c0cb686baec742f19bdcf004e9ca28098
Raghav <97241429+Stupid-Creations@users.noreply.github.com>
2024-08-31 16:35:22 +0530

    Update pixart.js

--- pixart.js
+++ pixart.js
@@ -8,35 +8,36 @@ const width = 125;
 const height = 125;
 
 setDocDimensions(width, height);
-let pixels = [".XXXXXX.",
-              ".XXXXXX.",
-              ".XXXXXX.",
-              ".XXXXXX.",
-              "........"]
+let pixels = [".....11........",
+              "....1111.22....",
+              "...111111212...",
+              "...111111222...",
+              "..111111122....",
+              "...22..22......",
+              "...22..22......",
+              "..............."]
 
 const makePixel = (x, y, brightness,size) => {
   let square = [[x,y],[x+size,y],[x+size,y+size],[x,y+size],[x,y]];
-  for(let i = 0;i < Math.round(size);i += size/(brightness/2)){
-    
-    let coord = i
-    square.push([x,Math.min(coord+y,size+y)],[coord+x,0+y]);
+  let lines = [];
+  let xs = [];
+  let ys = [];
+  let segments = Math.round(brightness/5);
+  for(let i = 0; i < segments+1;i+= Math.round(size/segments)){
+    xs.push(x);
+    ys.push(y+i);
+    xs.push(x+i);
+    ys.push(y);
+    xs.push(x+size-i);
+    ys.push(y+size);
+    xs.push(x+size);
+    ys.push(y+size-i);
   }
-  console.log("f");
-  //   for(let i = Math.round(size);i > Math.round(brightness/size);i -= size/(brightness/5)){
-  //   let coord = i;
-  //   console.log(i);
-  //   console.log([coord,y+size],[x+size,coord]);
-  //   square.push([x,size-coord],[size-coord,0+y]);
-  // }
 
-  for(let i = 0;i < Math.round(size);i += size/(brightness/5)){
-    if(Math.round(size/(brightness/5)<Math.round(size))){
-    let coord = i
-      console.log();
-    square.push([Math.min((size+x)-coord,y+size),y+size],[y+size,Math.min((size+x)-coord,y+size)]);
-    }
+  for(let i = 0; i < xs.length/2; i+=2){
+    lines.push([[xs[i],ys[i]],[xs[i+1],ys[i+1]]]);
   }
-  return square;
+  return [square,lines];
 }
   
 // store final lines here
@@ -51,18 +52,17 @@ const polyline = [
   [30, 90]
 ];
 
+console.log('f');
 for(let i = 0; i < pixels.length; i++){
-      console.log(i);
-      console.log("heyy");
   for(let j = 0; j < pixels[i].length; j++){
-    console.log("hey");
-    if(pixels[i][j] === "."){
-      drawLines([makePixel(j*5,i*5,0,10)]);
+    if(pixels[i][j] === "1"){
+      drawLines(bt.rotate([makePixel(j*10,i*10,0,10)[0]]);
+      drawLines(makePixel(j*10,i*10,100,10)[1]);
+    }
+        if(pixels[i][j] === "2"){
+      drawLines([makePixel(j*10,i*10,0,10)[0]]);
     }
   }
 }
-// add the polyline to the final lines
-// transform lines using the toolkit
 
-// draw i
-//drawLines([makePixel(4,4,1,10)]);
+
